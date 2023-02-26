925e12fc36e7ba46857b50b13526703176377367
Colette Ulianogit config --global --listclear <culiano@live.com>
2023-02-26 18:18:38 -0500

    completed assignment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Axios Pokemon API
 setting up a very basic list of Pokemon using their api and Axios
+
+Building off of the previous assignment which used fetch for the api call
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+    const [list, setList] = useState([]);
+
+    // * doesn't have to be an arrow function, old school way will work here too, easier for me to read
+    useEffect(function() {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        // * weird axios thing, need to include .data for some reason, or else none of the data will show
+        .then(response => setList(response.data.results))
+        .catch(err => console.log(err));
+    }, []);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="w-50 m-auto mt-4 mb-4 card p-3">
+            <h1>All the Pokemon</h1>
+            <ul>
+        {
+            list.map((single, idx) => (
+                <li key={idx}>{single.name}</li>
+            ))
+        }    
+            </ul>
         </div>
     );
 }
